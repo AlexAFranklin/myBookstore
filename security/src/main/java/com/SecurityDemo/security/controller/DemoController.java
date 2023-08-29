8b2f47c48b888fc5c1f85211948f61b3c62dfc04
Alex Franklin <alexandra.a.franklin@gmail.com>
2023-08-29 16:19:47 -0400

    figured out thymeleaf thing

--- security/src/main/java/com/SecurityDemo/security/controller/DemoController.java
+++ security/src/main/java/com/SecurityDemo/security/controller/DemoController.java
@@ -5,12 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
 public class DemoController {
     @GetMapping("")
-    public String sayHello(){
+    public String sayHello(Model model, Authentication authentication) {
+        String userName = authentication.getName();
+        List<String> roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        model.addAttribute("userName", userName);
+        model.addAttribute("roles", roles);
+
         return "hello";
     }
 
