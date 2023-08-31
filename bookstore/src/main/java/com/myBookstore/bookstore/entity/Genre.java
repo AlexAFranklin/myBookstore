aabe6e81fd7f52d4392bdc374e9c83c8b613bca4
Ehsan Khoshnavaz Motlagh <e.khoshnavaz@gmail.com>
2023-08-31 13:49:06 -0400

    customized text on Genre dispaly

--- bookstore/src/main/java/com/myBookstore/bookstore/entity/Genre.java
+++ bookstore/src/main/java/com/myBookstore/bookstore/entity/Genre.java
@@ -2,15 +2,25 @@ package com.myBookstore.bookstore.entity;
 
 public enum Genre {
 
-    NON_FICTION,
-    GRAPHIC_NOVEL,
-    FICTION,
-    MYSTERY_THRILLER,
-    ROMANCE,
-    SCIENCE_FICTION,
-    FANTASY,
-    CLASSIC,
-    HISTORICAL,
-    YOUNG_ADULT
+    NON_FICTION("Non Fiction"),
+    GRAPHIC_NOVEL("Graphic Novel"),
+    FICTION("Fiction"),
+    MYSTERY_THRILLER("Mystery Thriller"),
+    ROMANCE("Romance"),
+    SCIENCE_FICTION("Science Fiction"),
+    FANTASY("Fantasy"),
+    CLASSIC("Classic"),
+    HISTORICAL("Historical"),
+    YOUNG_ADULT("Young Adults");
+
+    private final String displayValue;
+
+    private Genre(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 
 }
